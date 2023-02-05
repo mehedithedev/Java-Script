23dbf08f9b880e11f6ca77a4a93f15efe48e1277
Mehedi Hasan <mehedih.dev@gmail.com>
2023-02-05 06:32:48 -0800

    init

---
+++ Practice/arraySome.js
@@ -0,0 +1,13 @@
+const arr=[1,2,3,4,5,6,7,8,9,10]
+
+// Array Some
+const someResult=arr.some((element)=>{
+    return element>4  // check if any element on the array is greeter than 4
+})
+console.log(someResult)
+
+// Array every
+const everyResult= arr.every((element_Every)=>{
+    return element_Every>2 // checks if every element on the array is greeter than 2
+})
+console.log(everyResult)
